d01b6864c1a76f98522ca55b43c243b186c0a351
Zaini Rejab <mairamohdrozaini@gmail.com>
2025-02-19 18:21:40 +0200

    Edit codes

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,13 +24,13 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Python, Javascript, Java, and C. </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">Web Technologies and Designs </b> and
                 also in areas related to{" "}
                 <b className="purple">
                   Blockchain.
